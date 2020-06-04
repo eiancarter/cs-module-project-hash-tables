940948c50701ad8350a948ea1dea4478c61903a4
Eian Carter <eian@bricleir.com>
2020-06-04 13:30:58 -0700

    added no dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,24 @@
 def no_dups(s):
     # Your code here
+    if s == "":
+        return ""
+    new_str_list = s.split(" ")
+    words = dict()
+    for word in new_str_list:
+        if word not in words:
+            words[word] = 1
+        else:
+            words[word] += 1
+    output_array = []
+    for word in words:
+        if words[word] == 1:
+            output_array.append(word)
+        output_str = (" ".join(output_array))
+    return output_str
+
+    
+
+
 
 
 
